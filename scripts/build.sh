c4c07af8dd9f223f5c1108b7f548543df7c7b052
lieff <lieff@users.noreply.github.com>
2018-01-10 17:50:25 +0300

    enhance compliance test

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,18 +4,16 @@ if [ ! -d "fate-suite.ffmpeg.org" ]; then
   wget -np -r --level=1 http://fate-suite.ffmpeg.org/mp3-conformance/
 fi
 
-gcc -Dminimp3_test -O2 -g -o minimp3 minimp3.c
+gcc -Dminimp3_test -O2 -g -o minimp3 minimp3.c -lm
 
 APP=./minimp3
 
 set +e
 for i in fate-suite.ffmpeg.org/mp3-conformance/*.bit; do
-$($APP $i $i.out)
+$APP $i $i.out ${i%.*}.pcm
 retval=$?
 echo $i exited with code=$retval
-cmp -s $i $i.out > /dev/null
-if [ $? -eq 1 ]; then
-    echo test failed
-    exit 1
+if [ ! $retval -eq 0 ]; then
+  exit 1
 fi
 done
