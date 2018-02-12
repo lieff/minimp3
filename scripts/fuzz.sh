4b8d2b738bdb227ef040b428dfa91eb4bb0fcfb7
lieff <lieff@users.noreply.github.com>
2018-02-13 01:41:03 +0300

    add fuzzing scripts

---
+++ scripts/fuzz.sh
@@ -0,0 +1,8 @@
+_FILENAME=${0##*/}
+CUR_DIR=${0/${_FILENAME}}
+CUR_DIR=$(cd $(dirname ${CUR_DIR}); pwd)/$(basename ${CUR_DIR})/
+
+pushd $CUR_DIR/..
+
+afl-clang-fast -fsanitize=address,undefined -fno-sanitize-recover=address,undefined -DFUZZ -o minimp3_fuzz minimp3_test.c -lm
+afl-fuzz -d -m none -i vectors/fuzz -o fuzz_out -- ./minimp3_fuzz @@
