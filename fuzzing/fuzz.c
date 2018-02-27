1d197b90eb5d17c123d449bd4286250ed8825d59
lieff <lieff@users.noreply.github.com>
2018-02-27 13:48:15 +0300

    fuzz: better stability

--- fuzzing/fuzz.c
+++ fuzzing/fuzz.c
@@ -6,24 +6,23 @@ int main()
 {
     static mp3dec_t mp3d;
     mp3dec_frame_info_t info;
-    int nbuf = 0;
     unsigned char buf[4096];
 
-    mp3dec_init(&mp3d);
-
 #ifdef __AFL_HAVE_MANUAL_CONTROL
-	__AFL_INIT();
+    __AFL_INIT();
     while (__AFL_LOOP(1000))
 #endif
-	{
-		do
-	    {
-	        short pcm[MINIMP3_MAX_SAMPLES_PER_FRAME];
-        	nbuf += fread(buf + nbuf, 1, sizeof(buf) - nbuf, stdin);
-	        mp3dec_decode_frame(&mp3d, buf, nbuf, pcm, &info);
-			nbuf -= info.frame_bytes;
-	    } while (info.frame_bytes);
-	}
+    {
+        int nbuf = 0;
+        mp3dec_init(&mp3d);
+        do
+        {
+            short pcm[MINIMP3_MAX_SAMPLES_PER_FRAME];
+            nbuf += fread(buf + nbuf, 1, sizeof(buf) - nbuf, stdin);
+            mp3dec_decode_frame(&mp3d, buf, nbuf, pcm, &info);
+            nbuf -= info.frame_bytes;
+        } while (info.frame_bytes);
+    }
 
     return 0;
 }
