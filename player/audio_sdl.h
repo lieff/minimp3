1ac3a4cdffcbdb76683cfa1b869600b44b71742f
lieff <lieff@users.noreply.github.com>
2018-04-27 15:36:48 +0300

    sample player WIP

--- player/audio_sdl.h
+++ player/audio_sdl.h
@@ -1,10 +1,12 @@
 #pragma once
+#include "decode.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 int sdl_audio_init(void **audio_render, int samplerate, int channels, int format, int buffer);
 void sdl_audio_release(void *audio_render);
+void sdl_audio_set_dec(void *audio_render, decoder *dec);
 
 #ifdef __cplusplus
 }
--- player/decode.h
+++ player/decode.h
@@ -1,22 +1,31 @@
 #pragma once
-#include "system.h"
+#include <stdint.h>
+#include "../minimp3.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+typedef int (*PARSE_GET_FILE_CB)(void *user, char **file_name);
+typedef int (*PARSE_INFO_CB)(void *user, char *file_name, int rate, int mp3_channels, float duration);
+
 typedef struct decoder
 {
-    CRITICAL_SECTION mp3_lock;
-    const char *file_name;
-    short *mp3_buf;
-    int mp3_size, mp3_pos, mp3_allocated, mp3_file_size, mp3_rate, mp3_channels;
+    mp3dec_t mp3d;
+    mp3dec_frame_info_t info;
+    uint8_t *mp3_buf;
+    size_t mp3_size, pos;
+    int file, pcm_bytes, pcm_copied, mp3_rate, mp3_channels;
     float mp3_duration;
-    HANDLE mp3_open_thread;
+    float spectrum[32][2]; // for visualization
+    short pcm[MINIMP3_MAX_SAMPLES_PER_FRAME];
 } decoder;
 
 extern decoder _dec;
 
-int preload_mp3(decoder *dec, const char *file_name);
+int open_dec(decoder *dec, const char *file_name);
+int close_dec(decoder *dec);
+void decode_samples(decoder *dec, uint8_t *buf, int bytes);
+void start_parser(PARSE_GET_FILE_CB fcb, void *fcb_user, PARSE_INFO_CB icb, void *icb_user);
 
 #ifdef __cplusplus
 }
